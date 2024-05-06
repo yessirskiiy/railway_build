f55403f50bb256a852d767692c95d0ad606df6aa
Антон Езерский <anton030701@gmail.com>
2024-05-06 17:56:37 +0300

    celery repair4

--- generator/settings.py
+++ generator/settings.py
@@ -135,7 +135,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [(os.environ.get("REDIS_URL"), 6379)],
+            "hosts": [os.environ.get("REDIS_URL")],
         },
     }
 }
